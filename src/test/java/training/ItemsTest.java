4092c76d3ac1f385d4ab8288e0d33a4a181bc087
asim516 <57764162+asim516@users.noreply.github.com>
2020-04-04 23:49:07 +0530

    Add files via upload

---
+++ src/test/java/training/ItemsTest.java
@@ -0,0 +1,14 @@
+package training;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ItemsTest {
+
+    @Test
+    public void putItem() {
+
+        Items obj = new Items();
+        Assert.assertNotNull(obj);
+    }
+}
