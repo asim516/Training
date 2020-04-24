7ca6c00ca1717a19ee85dd1eab282aef21cefa09
asim516 <57764162+asim516@users.noreply.github.com>
2020-04-25 01:43:55 +0530

    Add files via upload

--- src/main/java/training/App.java
+++ src/main/java/training/App.java
@@ -4,24 +4,24 @@
  */
 package training;
 
-
 public class App {
 
-    public String getGreeting() {
-        return "Hello consortium world.";
-    }
-
     public static void main(String[] args) throws InterruptedException {
-       
 
-ItemsCreator item=new ItemsCreator();
 
-      item.putItem();
+        ItemsCreator item = new ItemsCreator();
+
+        item.putItem();
         System.out.println(new App().getGreeting());
 
         System.out.println(new Area().getAreaforAllShapes());
         System.out.print(new Employees().obj.toJSONString());
+       AddingData add= new AddingData();
+       add.save();
 
+    }
 
-            }
+    public String getGreeting() {
+        return "Hello consortium world.";
+    }
 }
